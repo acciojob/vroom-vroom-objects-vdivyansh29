9d1f212cb12092f6e85dc5c94e3e1319bd7528e1
Divyansh Verma <divyanshmotog5g@gmail.com>
2025-08-28 13:37:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	if(typeof make !== "string" || typeof model !== "string"){
+        throw new Error("make and model should be string")
+    }
+    this.make = make
+    this.model = model
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel = function (){
+    return `${this.make} ${this.model}`
+}
 
+function SportsCar(make, model, topSpeed) {
+	this.make = make
+    this.model = model
+    this.topSpeed = topSpeed
+}
+
+SportsCar.prototype = Object.create(Car.prototype)
+
+SportsCar.prototype.getTopSpeed = function(){
+    this.topSpeed
+}
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
